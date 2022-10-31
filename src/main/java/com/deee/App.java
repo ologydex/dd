d015753a85bbceac224a81f355b999cedd407278
deji <deji@gmail.com>
2022-10-31 20:53:47 +0000

    pom.xml and src files are added

---
+++ src/main/java/com/deee/App.java
@@ -0,0 +1,13 @@
+package com.deee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello maven!" );
+    }
+}
